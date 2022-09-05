62f7d0bd36fde73ee94aa47eb76c9f5fcd099827
mrcabbage972 <mayvic@gmail.com>
2022-09-04 21:50:34 -0400

    Fixing logging

--- src/kaggle_ell/solutions/transformer_finetune.py
+++ src/kaggle_ell/solutions/transformer_finetune.py
@@ -1,14 +1,15 @@
+from typing import Mapping
+
+import pandas as pd
+
 from kaggle_ell.solution_factory import SolutionFactory
 from kaggle_ell.solution import Solution
 
 
 @SolutionFactory.register('transformer_finetune')
 class TransformerFinetune(Solution):
-    def train(self, input_data_path: str, artifacts_pat: str):
+    def do_train(self, train_data: pd.DataFrame, train_cfg: Mapping, model_cfg: Mapping, artifacts_path: str):
         pass
 
-    def predict(self, input_data_path: str, artifacts_path: str):
-        pass
-
-    def create_submission(self, test_preds, output_dir: str):
+    def do_predict(self, input_data: pd.DataFrame, inference_cfg: Mapping, artifacts_path: str):
         pass
